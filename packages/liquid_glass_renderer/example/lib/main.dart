d74e754f93083ccc4633cfd601d5dc21855b84a9
Tim Lehmann <tim.lehmann@loql.com>
2025-06-13 19:18:22 +0200

    docs: made example prettier

--- packages/liquid_glass_renderer/example/lib/main.dart
+++ packages/liquid_glass_renderer/example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 import 'package:rivership/rivership.dart';
 import 'package:smooth_sheets/smooth_sheets.dart';
@@ -11,15 +12,17 @@ void main() {
   runApp(const MainApp());
 }
 
-final thicknessNotifier = ValueNotifier<double>(30);
+final thicknessNotifier = ValueNotifier<double>(20);
 
-final blurFactorNotifier = ValueNotifier<double>(0.01);
+final blurFactorNotifier = ValueNotifier<double>(.02);
 
-final cornerRadiusNotifier = ValueNotifier<double>(50);
+final cornerRadiusNotifier = ValueNotifier<double>(100);
 
-final glassColorNotifier = ValueNotifier<Color>(Colors.transparent);
+final glassColorNotifier = ValueNotifier<Color>(
+  const Color.fromARGB(33, 255, 255, 255),
+);
 
-final lightIntensityNotifier = ValueNotifier<double>(0.4);
+final lightIntensityNotifier = ValueNotifier<double>(1);
 
 final blendNotifier = ValueNotifier<double>(50);
 
@@ -61,13 +64,23 @@ class MainApp extends HookWidget {
       ),
     )!;
 
+    final colorScheme = ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: Color(0xFF287390),
+    );
     return MaterialApp(
-      home: Builder(
-        builder: (context) {
-          return Scaffold(
-            extendBodyBehindAppBar: true,
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
+      theme: ThemeData.from(
+        colorScheme: colorScheme,
+        textTheme: GoogleFonts.lexendDecaTextTheme().apply(
+          displayColor: colorScheme.onSurface,
+          bodyColor: colorScheme.onSurface,
+        ),
+      ),
+      home: Scaffold(
+        body: Builder(
+          builder: (context) {
+            return GestureDetector(
+              onTap: () {
                 Navigator.push(
                   context,
                   ModalSheetRoute(
@@ -79,85 +92,148 @@ class MainApp extends HookWidget {
                   ),
                 );
               },
-
-              child: const Icon(Icons.color_lens),
-            ),
-
-            body: GestureDetector(
-              onTap: () {
-                thicknessVisible.value = !thicknessVisible.value;
-              },
-
-              child: Stack(
-                children: [
-                  Positioned.fill(child: Container(color: Colors.transparent)),
-                  Positioned.fill(
-                    child: Image.asset(
-                      'assets/wallpaper.webp',
-                      fit: BoxFit.cover,
+              child: Background(
+                child: LiquidGlassLayer(
+                  settings: LiquidGlassSettings(
+                    thickness: thickness,
+                    lightAngle: lightAngle,
+                    glassColor: color.withValues(
+                      alpha: color.a * thickness / 20,
                     ),
+                    lightIntensity: lightIntensityNotifier.value,
+                    ambientStrength: 0.8,
+                    outlineIntensity: 2,
+                    blend: blend,
                   ),
-
-                  Positioned.fill(
-                    child: LiquidGlassLayer(
-                      settings: LiquidGlassSettings(
-                        thickness: thickness,
-                        lightAngle: lightAngle,
-                        glassColor: color.withValues(
-                          alpha: color.a * thickness / 20,
+                  child: Stack(
+                    alignment: Alignment.bottomLeft,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 90, left: 140),
+                        child: DragDismissable(
+                          threshold: double.maxFinite,
+                          velocityThreshold: double.maxFinite,
+                          spring: Spring.bouncy,
+                          child: LiquidGlass.inLayer(
+                            blur: blur,
+                            shape: LiquidGlassSquircle(
+                              borderRadius: Radius.circular(cornerRadius),
+                            ),
+                            child: Container(
+                              color: Colors.transparent,
+                              child: SizedBox.square(dimension: 160),
+                            ),
+                          ),
                         ),
-                        lightIntensity: lightIntensityNotifier.value,
-                        blend: blend,
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        spacing: 40,
-                        children: [
-                          Align(
-                            alignment: Alignment.topCenter,
-                            child: DragDismissable(
-                              threshold: double.maxFinite,
-                              velocityThreshold: double.maxFinite,
-                              spring: Spring.bouncy,
-                              child: LiquidGlass.inLayer(
-                                blur: blur,
-                                shape: LiquidGlassSquircle(
-                                  borderRadius: Radius.circular(cornerRadius),
-                                ),
-                                child: Container(
-                                  color: Colors.transparent,
-                                  child: SizedBox.square(dimension: 100),
-                                ),
-                              ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: DragDismissable(
+                          threshold: double.maxFinite,
+                          velocityThreshold: double.maxFinite,
+                          spring: Spring.bouncy,
+                          child: LiquidGlass.inLayer(
+                            glassContainsChild: false,
+                            blur: blur,
+                            shape: LiquidGlassSquircle(
+                              borderRadius: Radius.circular(cornerRadius),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(64.0),
+                              child: FlutterLogo(size: 200),
                             ),
                           ),
-                          Center(
-                            child: DragDismissable(
-                              threshold: double.maxFinite,
-                              velocityThreshold: double.maxFinite,
-                              spring: Spring.bouncy,
-                              child: LiquidGlass.inLayer(
-                                glassContainsChild: false,
-                                blur: blur,
-                                shape: LiquidGlassSquircle(
-                                  borderRadius: Radius.circular(cornerRadius),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: FlutterLogo(size: 200),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends HookWidget {
+  const Background({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final showHint = useDelayed(
+      delay: Duration(seconds: 1),
+      before: false,
+      after: true,
+    );
+    useEffect(() {
+      if (showHint) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Drag Glass or tap anywhere!",
+                style: GoogleFonts.lexendDecaTextTheme().bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onInverseSurface,
+                ),
               ),
             ),
           );
-        },
+        });
+      }
+      return null;
+    }, [showHint]);
+    return SizedBox.expand(
+      child: Container(
+        color: Theme.of(context).colorScheme.surface,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 64, left: 64),
+          decoration: ShapeDecoration(
+            color: Theme.of(context).colorScheme.surfaceContainerHighest,
+            shape: RoundedSuperellipseBorder(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
+            ),
+          ),
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 16, left: 16),
+            decoration: ShapeDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/wallpaper.webp'),
+                fit: BoxFit.cover,
+              ),
+              shape: RoundedSuperellipseBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(64),
+                ),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(64.0),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      'Liquid\nGlass\nRenderer',
+                      style: GoogleFonts.lexendDecaTextTheme().headlineLarge
+                          ?.copyWith(
+                            fontSize: 120,
+                            height: 1,
+                            fontWeight: FontWeight.w900,
+                            color: Color(0xFF287390),
+                          ),
+                    ),
+                  ),
+                  child,
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -169,11 +245,12 @@ class SettingsSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Sheet(
+      dragConfiguration: SheetDragConfiguration(),
       scrollConfiguration: const SheetScrollConfiguration(),
-      initialOffset: SheetOffset(0.5),
+      initialOffset: SheetOffset(1),
       snapGrid: SheetSnapGrid(snaps: [SheetOffset(0.5), SheetOffset(1)]),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(32.0),
         child: SafeArea(
           child: LiquidGlass(
             blur: 20,
