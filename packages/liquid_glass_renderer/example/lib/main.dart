ea5702f07a49fb2f2243374fc9941004b4fefedc
Tim Lehmann <tim.lehmann@loql.com>
2025-07-23 14:59:05 +0200

    test: try fix golden tests

--- packages/liquid_glass_renderer/example/lib/main.dart
+++ packages/liquid_glass_renderer/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,9 +14,10 @@ void main() {
 
 final settingsNotifier = ValueNotifier<LiquidGlassSettings>(
   LiquidGlassSettings(
-    ambientStrength: 0,
-    chromaticAberration: 0,
-    lightIntensity: 0,
+    thickness: 20,
+    lightAngle: 0.5 * pi,
+    blend: 50,
+    chromaticAberration: 1,
   ),
 );
 
@@ -81,6 +84,7 @@ class MainApp extends HookWidget {
                   settingsNotifier: settingsNotifier,
                   lightAngleAnimation: lightAngleController,
                 ).show(context),
+
                 child: Background(
                   lightAngle: lightAngle,
                   textVisible: thicknessVisible.value,
