0cafaaf00de98b673abdda0d4e8f29601e108bfe
Tim Lehmann <hi@timcreated.it>
2025-10-09 14:51:28 +0200

    linter love

--- packages/liquid_glass_renderer/lib/liquid_glass_renderer.dart
+++ packages/liquid_glass_renderer/lib/liquid_glass_renderer.dart
@@ -2,7 +2,6 @@
 library liquid_glass_renderer;
 
 export 'src/fake_glass.dart' show FakeGlass;
-export 'src/stretch.dart' show LiquidStretch;
 export 'src/glass_glow.dart' show GlassGlow, GlassGlowLayer;
 export 'src/glass_link.dart' show GlassLink;
 export 'src/glassify.dart' show Glassify;
@@ -10,3 +9,4 @@ export 'src/liquid_glass.dart' show LiquidGlass;
 export 'src/liquid_glass_layer.dart' show LiquidGlassLayer;
 export 'src/liquid_glass_settings.dart' show LiquidGlassSettings;
 export 'src/liquid_shape.dart';
+export 'src/stretch.dart' show LiquidStretch;
